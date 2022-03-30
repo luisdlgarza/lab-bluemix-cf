a823c8687b590eb5f82346b7a3eff7aaf67d60be
luisdlgarza <luisdlgarza2@gmail.com>
2022-03-30 00:07:03 +0000

    Weird Modificatons

--- scripts/linux-tools.sh
+++ scripts/linux-tools.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # Add the IBM Cloud CLI if not ARM architecture
-if [ $(uname -m) != aarch64 ]; then
+if [ $(uname -m) != aarch64 ]
+then
     echo "Installing IBM Cloud CLI..."
     curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
     echo "alias ic=ibmcloud" >> /home/devops/.bashrc
     echo "Installing Cloud Foundry CLI..."
     ibmcloud cf install -f
-else \
+else
     echo "Sorry IBM Cloud does not support aarch64 architecture :("
 fi;
